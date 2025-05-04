a1557c08c39fa5ff4480db86a19afddb061fd5a8
Kristenz24 <kristenzmingoy@gmail.com>
2025-05-05 00:05:27 +0800

    updated UI

--- QR_PASS_FE/assets/scripts/utils/animation.js
+++ QR_PASS_FE/assets/scripts/utils/animation.js
@@ -1,11 +1,12 @@
 // Onload animation using GSAP
 window.onload = function () {
-    gsap.from([".hero-container", ".cards-container"], {
-      y: 50,
-      opacity: 0,
-      duration: 1,
-      ease: "power2.out",
-      stagger: 0.3,
-      delay: 0.5
-    });
+  gsap.from([".hero-container", ".cards-container"], {
+    y: 50,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.3,
+    delay: 0.5
+  });
 };
+
